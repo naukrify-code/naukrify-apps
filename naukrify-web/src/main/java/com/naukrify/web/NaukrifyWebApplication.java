2fa0f5181c3891643cfb6cf1bb184154da97caca
Tamilselvan <tamilselvan.coder@gmail.com>
2022-05-17 07:49:23 +0530

    project generated by spring boot

---
+++ naukrify-web/src/main/java/com/naukrify/web/NaukrifyWebApplication.java
@@ -0,0 +1,13 @@
+package com.naukrify.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukrifyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaukrifyWebApplication.class, args);
+	}
+
+}
---
+++ naukrify-web/src/test/java/com/naukrify/web/NaukrifyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naukrify.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukrifyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
